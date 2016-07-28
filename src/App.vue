a43fbb27ace57abfa1929dd56a40b54a8045b014
daniel-schmitz <danieljfa@gmail.com>
2016-07-28 14:24:07 -0300

    Simple views

--- src/App.vue
+++ src/App.vue
@@ -5,50 +5,12 @@
       <tab name="t1" title="Tab 1" active=true>Hello World</tab>
       <tab name="t2" title="Tab 2">
 
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>John</td>
-              <td>Doe</td>
-              <td>john@example.com</td>
-            </tr>
-            <tr>
-              <td>Mary</td>
-              <td>Moe</td>
-              <td>mary@example.com</td>
-            </tr>
-            <tr>
-              <td>July</td>
-              <td>Dooley</td>
-              <td>july@example.com</td>
-            </tr>
-          </tbody>
-        </table>
+       <simple-table></simple-table>
 
       </tab>
       <tab name="t3" title="Tab 3">
 
-       <form role="form">
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter email">
-        </div>
-        <div class="form-group">
-          <label for="pwd">Password:</label>
-          <input type="password" class="form-control" id="pwd" placeholder="Enter password">
-        </div>
-        <div class="checkbox">
-          <label><input type="checkbox"> Remember me</label>
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+      <simple-form></simple-form>
 
     </tab>
   </tabs>
@@ -57,27 +19,14 @@
 <script>
   import Tabs from './components/Tabs.vue'
   import Tab from './components/Tab.vue'
+  import SimpleTable from './views/SimpleTable.vue'
+  import SimpleForm from './views/SimpleForm.vue'
+
   export default {
     components: {
-      Tabs,
-      Tab
+      Tabs,Tab,
+      SimpleForm,SimpleTable
     }
   }
 </script>
-<style>
-  .nav-tabs {
-    background-color: #eeeeee;
-    padding-top:5px;
-    padding-left:5px;
-    border: 1px solid #ddd;
 
-  }
-
-  .tab-content {
-    color : #000000;
-    background-color: #ffffff;
-    padding : 5px 15px;
-    margin-top: -1px;
-    border: 1px solid #ddd;
-  }
-</style>
