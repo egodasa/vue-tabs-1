13f05fd40f43eb189c94d1071c9839829dd2a783
daniel-schmitz <danieljfa@gmail.com>
2016-07-28 12:48:44 -0300

    move style to tabs component

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -26,3 +26,20 @@
       }
   }
 </script>
+<style>
+  .nav-tabs {
+    background-color: #eeeeee;
+    padding-top:5px;
+    padding-left:5px;
+    border: 1px solid #ddd;
+
+  }
+
+  .tab-content {
+    color : #000000;
+    background-color: #ffffff;
+    padding : 5px 15px;
+    margin-top: -1px;
+    border: 1px solid #ddd;
+  }
+</style>
