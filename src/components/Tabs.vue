de60dbf07b90de2c68d1de8e84104a00f39c26f8
Daniel Schmitz <danieljfa@gmail.com>
2016-08-08 13:09:55 -0300

    Removendo iten desnecessário

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="nav nav-tabs">
         <li role="presentation" :class="{'active':tab.active}" v-for="tab in tablist" ><a href="#{{tab.name}}" aria-controls="{{tab.name}}" role="tab" data-toggle="tab">{{tab.title}}</a></li>
-    </ul>  
+    </ul>
     <div class="tab-content">
         <slot></slot>
     </div>
@@ -14,10 +14,8 @@
           }
       },
       ready(){
-        //console.log(this.$children);
-        let self = this;
         this.$children.forEach(c=>{
-            self.tablist.push({
+            this.tablist.push({
                     "name": c.name,
                     "title": c.title,
                     "active": c.active
